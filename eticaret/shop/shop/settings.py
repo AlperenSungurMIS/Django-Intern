729ca3e1bcbe98275a9c197286fe1e25874abe2e
Alperen sungur <97175131+AlperenSungurMIS@users.noreply.github.com>
2024-09-25 16:39:13 +0300

    Django-Intern

--- eticaret/shop/shop/settings.py
+++ eticaret/shop/shop/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import MEDIA_ROOT
+from django.forms import Media
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- eticaret/shop/urunler/admin.py
+++ eticaret/shop/urunler/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from .models import Kategoriler
+from django.contrib.admin import ModelAdmin
+from.models import Kategoriler
+from .models import Kategoriler,Markalar
 
 # Register your models here.
-admin.site.register(Kategoriler)
+
+class KategoriAdmin(admin.ModelAdmin):
+    list_display = ['isim', 'seo_title', 'slug', 'aktifmi']
+    list_filter = ['aktifmi','isim']
+    search_fields = ['isim', 'seo_title', 'slug']
+
+
+admin.site.register(Kategoriler,KategoriAdmin)
+class MarkalarAdmin(admin.ModelAdmin):
+    list_display = ['isim', 'aktifmi','slug','seo_title','resim']
+    list_filter = ['aktifmi', 'isim']
+    search_fields = ['isim', 'seo_title',  'slug']
+
+admin.site.register(Markalar,MarkalarAdmin)
