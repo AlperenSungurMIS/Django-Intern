fe385ac83e6415c1094e08826bad3aa2fea0890c
Alperen sungur <97175131+AlperenSungurMIS@users.noreply.github.com>
2024-09-27 16:50:22 +0300

    Django Intern

--- eticaret/shop/shop/settings.py
+++ eticaret/shop/shop/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS': [BASE_DIR / 'urunler' / 'templates'],  # Bu satırı ekleyin
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- eticaret/shop/urunler/views.py
+++ eticaret/shop/urunler/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Etiketler,Urunler,Kategoriler,Markalar
 
 # Create your views here.
+
+def anasayfa(request):
+    urunler=Urunler.objects.all()
+    return render(request, "index.html",{"urunler":urunler})
+
